008d14440790a2b0c3e1992e0339bad6ad6962d8
Henrique Papa <henrique_ahg@hotmail.com>
2021-04-12 23:19:29 -0300

    add lib

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='papalib',
+      version='0.1',
+      url='',
+      license='MIT',
+      author='Henrique Papa',
+      author_email='Henrique Papa A. Fonseca',
+      description='Função padrão para criação de libs',
+      packages=['papalib'],
+      long_description=open('README.md').read(),
+      zip_safe=False)
